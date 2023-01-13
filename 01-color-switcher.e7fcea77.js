14a9d9625232fc479484ed59d515029739bdfaaf
ArtemS25 <110459689+ArtemS25@users.noreply.github.com>
2023-01-13 14:21:32 +0000

    Deploying to gh-pages from @ ArtemS25/goit-js-hw-10@2ed69db4721431b3d3dbd7bd1d168a88996ea390 🚀

---
+++ 01-color-switcher.e7fcea77.js
@@ -0,0 +1,2 @@
+const t={start:document.querySelector("[data-start]"),stop:document.querySelector("[data-stop]")};let e=null;t.stop.setAttribute("disabled","true"),t.start.addEventListener("click",(function(){e=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.start.toggleAttribute("disabled"),t.stop.toggleAttribute("disabled")})),t.stop.addEventListener("click",(function(){t.start.toggleAttribute("disabled"),t.stop.toggleAttribute("disabled"),clearInterval(e)}));
+//# sourceMappingURL=01-color-switcher.e7fcea77.js.map
